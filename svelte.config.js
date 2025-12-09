31ad7ef65efff80336897f4613027ab39d749608
Agni Bhattacharya <agnx13@proton.me>
2025-12-09 17:32:18 +0530

    Repo name changed

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/arboreum_v1-main' : '',
+			base: process.env.NODE_ENV === 'production' ? '/arboreum_v2' : '',
 		}
 	}
 };
